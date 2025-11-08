530df366a478cefc59da5b63cc94e591a295a5c7
jiahuigeng <jiahui.geng@rwth-aachen.de>
2025-11-08 10:37:28 +0400

    update train

--- experiments_mmcoir/public/train/train_v2-qwen2vl-2B_imageonly.sh
+++ experiments_mmcoir/public/train/train_v2-qwen2vl-2B_imageonly.sh
@@ -14,19 +14,62 @@ echo "Python version: $(python --version)"
 # export HUGGING_FACE_HUB_TOKEN=...
 # export WANDB_PROJECT=...
 # export WANDB_RUN_GROUP=...
-export EXP_NAME=Qwen2vl_2B.image+visdoc+video.autoresize.lora16.BS1024.IB64.GCq8p8.NormTemp002.lr5e5.step5kwarm100.8H100
+############################################################
+# Configurable parameters (used to auto-name EXP_NAME)
+############################################################
+MODEL_TAG=Qwen2vl_2B
+MODEL_NAME=Qwen/Qwen2-VL-2B-Instruct
 
-export WANDB_NAME=$EXP_NAME
-export EXP_DIR=.../$EXP_NAME
-export WANDB_DIR=$EXP_DIR
-echo $EXP_DIR
+CUDA_VISIBLE_DEVICES=0,1
+NPROC_PER_NODE=2
+MASTER_PORT=2207
 
-mkdir -p $EXP_DIR/wandb
-rm -rf $EXP_DIR/wandb/*
+LORA=true
+LORA_R=8
+BF16=true
+POOLING=eos
+NORMALIZE=True
+TEMPERATURE=0.02
+DATALOADER_NUM_WORKERS=8
+DATASET_CONFIG=experiments_mmcoir/public/train/image.yaml
+DATASET_TAG=$(basename "$DATASET_CONFIG" .yaml)
+
+PER_DEVICE_TRAIN_BATCH_SIZE=16
+GC_Q_CHUNK_SIZE=4
+GC_P_CHUNK_SIZE=48
+INTERLEAVE_BATCH_SIZE=16
+LR_SCHEDULER_TYPE=linear
+LEARNING_RATE=5e-5
+MAX_STEPS=5000
+WARMUP_STEPS=100
+SAVE_STEPS=50
+LOGGING_STEPS=1
+SAVE_SAFETENSORS=True
+REMOVE_UNUSED_COLUMNS=False
+RESUME_FROM=auto
+REPORT_TO=wandb
+
+# Task tag for visibility in experiment name
+TASK_TAG=imageonly
+
+# Auto-construct experiment name based on parameters above
+# Format temperature to compact string (e.g., 0.02 -> 002)
+TEMP_FMT=$(printf "%03d" $(echo "${TEMPERATURE} * 100" | bc | awk '{printf("%d", $1)}'))
+EXP_NAME="${MODEL_TAG}.${TASK_TAG}.${DATASET_TAG}.lora${LORA_R}.bf16${BF16}.BS$((PER_DEVICE_TRAIN_BATCH_SIZE*NPROC_PER_NODE)).IB${INTERLEAVE_BATCH_SIZE}.GCq${GC_Q_CHUNK_SIZE}p${GC_P_CHUNK_SIZE}.Norm${NORMALIZE}.Temp${TEMP_FMT}.lr${LEARNING_RATE}.${LR_SCHEDULER_TYPE}.step${MAX_STEPS}warm${WARMUP_STEPS}"
+
+export WANDB_NAME="$EXP_NAME"
+EXP_DIR_BASE=...
+export EXP_DIR="$EXP_DIR_BASE/$EXP_NAME"
+export WANDB_DIR="$EXP_DIR"
+echo "$EXP_DIR"
+
+mkdir -p "$EXP_DIR/wandb"
+rm -rf "$EXP_DIR/wandb/*"
 
 
-cd PATH_TO_VLM2VEC_REPO
-cmd="CUDA_VISIBLE_DEVICES=0,1 torchrun --nproc_per_node=2 --master_port=2207 --max_restarts=0 train.py --lora --lora_r 8 --model_name Qwen/Qwen2-VL-2B-Instruct --bf16 --pooling eos --normalize True --temperature 0.02 --dataloader_num_workers 8 --dataset_config experiments/public/train/train_image.yaml  --run_name $EXP_NAME --output_dir $EXP_DIR --grad_cache True --per_device_train_batch_size 16 --gc_q_chunk_size 4 --gc_p_chunk_size 48 --interleave_batch_size 16 --lr_scheduler_type linear --learning_rate 5e-5 --max_steps 5000 --warmup_steps 100 --save_steps 50 --logging_steps 1 --save_safetensors True --remove_unused_columns False --resume_from auto --report_to wandb 2>&1 | tee $EXP_DIR/train.log"
+REPO_DIR=PATH_TO_VLM2VEC_REPO
+cd "$REPO_DIR"
+cmd="CUDA_VISIBLE_DEVICES=${CUDA_VISIBLE_DEVICES} torchrun --nproc_per_node=${NPROC_PER_NODE} --master_port=${MASTER_PORT} --max_restarts=0 train.py $( [ \"${LORA}\" = \"true\" ] && echo --lora ) --lora_r ${LORA_R} --model_name ${MODEL_NAME} $( [ \"${BF16}\" = \"true\" ] && echo --bf16 ) --pooling ${POOLING} --normalize ${NORMALIZE} --temperature ${TEMPERATURE} --dataloader_num_workers ${DATALOADER_NUM_WORKERS} --dataset_config ${DATASET_CONFIG} --run_name ${EXP_NAME} --output_dir ${EXP_DIR} --grad_cache True --per_device_train_batch_size ${PER_DEVICE_TRAIN_BATCH_SIZE} --gc_q_chunk_size ${GC_Q_CHUNK_SIZE} --gc_p_chunk_size ${GC_P_CHUNK_SIZE} --interleave_batch_size ${INTERLEAVE_BATCH_SIZE} --lr_scheduler_type ${LR_SCHEDULER_TYPE} --learning_rate ${LEARNING_RATE} --max_steps ${MAX_STEPS} --warmup_steps ${WARMUP_STEPS} --save_steps ${SAVE_STEPS} --logging_steps ${LOGGING_STEPS} --save_safetensors ${SAVE_SAFETENSORS} --remove_unused_columns ${REMOVE_UNUSED_COLUMNS} --resume_from ${RESUME_FROM} --report_to ${REPORT_TO} 2>&1 | tee ${EXP_DIR}/train.log"
 
-echo $cmd
-eval $cmd
+echo "$cmd"
+eval "$cmd"
