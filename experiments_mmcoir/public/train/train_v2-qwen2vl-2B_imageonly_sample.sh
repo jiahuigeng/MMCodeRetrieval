ceed0ee04a5fa24552d3f4684ba51816718faf08
jiahuigeng <jiahui.geng@rwth-aachen.de>
2025-11-08 14:32:36 +0400

    small eval

--- experiments_mmcoir/public/train/train_v2-qwen2vl-2B_imageonly_sample.sh
+++ experiments_mmcoir/public/train/train_v2-qwen2vl-2B_imageonly_sample.sh
@@ -40,9 +40,12 @@ GC_P_CHUNK_SIZE=48
 INTERLEAVE_BATCH_SIZE=16
 LR_SCHEDULER_TYPE=linear
 LEARNING_RATE=5e-5
-MAX_STEPS=5000
-WARMUP_STEPS=100
-SAVE_STEPS=50
+MAX_STEPS=200
+WARMUP_STEPS=20
+# 仅用于按 epoch 控制训练长度；当 MAX_STEPS>0 时以 steps 为准
+NUM_TRAIN_EPOCHS=1
+# 将保存间隔设为 MAX_STEPS，只在接近末尾保存一次，减少 I/O
+SAVE_STEPS=${MAX_STEPS}
 LOGGING_STEPS=1
 SAVE_SAFETENSORS=True
 REMOVE_UNUSED_COLUMNS=False
@@ -70,7 +73,7 @@ rm -rf "$EXP_DIR/wandb/*"
 # 修改为你的仓库根目录（包含 train.py）
 REPO_DIR=PATH_TO_VLM2VEC_REPO
 cd "$REPO_DIR"
-cmd="CUDA_VISIBLE_DEVICES=${CUDA_VISIBLE_DEVICES} torchrun --nproc_per_node=${NPROC_PER_NODE} --master_port=${MASTER_PORT} --max_restarts=0 train.py $( [ \"${LORA}\" = \"true\" ] && echo --lora ) --lora_r ${LORA_R} --model_name ${MODEL_NAME} $( [ \"${BF16}\" = \"true\" ] && echo --bf16 ) --pooling ${POOLING} --normalize ${NORMALIZE} --temperature ${TEMPERATURE} --dataloader_num_workers ${DATALOADER_NUM_WORKERS} --dataset_config ${DATASET_CONFIG} --run_name ${EXP_NAME} --output_dir ${EXP_DIR} --grad_cache True --per_device_train_batch_size ${PER_DEVICE_TRAIN_BATCH_SIZE} --gc_q_chunk_size ${GC_Q_CHUNK_SIZE} --gc_p_chunk_size ${GC_P_CHUNK_SIZE} --interleave_batch_size ${INTERLEAVE_BATCH_SIZE} --lr_scheduler_type ${LR_SCHEDULER_TYPE} --learning_rate ${LEARNING_RATE} --max_steps ${MAX_STEPS} --warmup_steps ${WARMUP_STEPS} --save_steps ${SAVE_STEPS} --logging_steps ${LOGGING_STEPS} --save_safetensors ${SAVE_SAFETENSORS} --remove_unused_columns ${REMOVE_UNUSED_COLUMNS} --resume_from ${RESUME_FROM} --report_to ${REPORT_TO} 2>&1 | tee ${EXP_DIR}/train.log"
+cmd="CUDA_VISIBLE_DEVICES=${CUDA_VISIBLE_DEVICES} torchrun --nproc_per_node=${NPROC_PER_NODE} --master_port=${MASTER_PORT} --max_restarts=0 train.py $( [ \"${LORA}\" = \"true\" ] && echo --lora ) --lora_r ${LORA_R} --model_name ${MODEL_NAME} $( [ \"${BF16}\" = \"true\" ] && echo --bf16 ) --pooling ${POOLING} --normalize ${NORMALIZE} --temperature ${TEMPERATURE} --dataloader_num_workers ${DATALOADER_NUM_WORKERS} --dataset_config ${DATASET_CONFIG} --run_name ${EXP_NAME} --output_dir ${EXP_DIR} --grad_cache True --per_device_train_batch_size ${PER_DEVICE_TRAIN_BATCH_SIZE} --gc_q_chunk_size ${GC_Q_CHUNK_SIZE} --gc_p_chunk_size ${GC_P_CHUNK_SIZE} --interleave_batch_size ${INTERLEAVE_BATCH_SIZE} --lr_scheduler_type ${LR_SCHEDULER_TYPE} --learning_rate ${LEARNING_RATE} --max_steps ${MAX_STEPS} --num_train_epochs ${NUM_TRAIN_EPOCHS} --warmup_steps ${WARMUP_STEPS} --save_steps ${SAVE_STEPS} --logging_steps ${LOGGING_STEPS} --save_safetensors ${SAVE_SAFETENSORS} --remove_unused_columns ${REMOVE_UNUSED_COLUMNS} --resume_from ${RESUME_FROM} --report_to ${REPORT_TO} 2>&1 | tee ${EXP_DIR}/train.log"
 
 echo "$cmd"
 eval "$cmd"
